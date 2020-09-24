70cea3675d2e8a676ba63c680eff226fdfb848c2
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-09-24 13:45:46 +0700

    get state

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <div>
+      <ul>
+        <li v-for="product in products" :key="product.name">
+          {{ product.name }}, ${{ product.price }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    products() {
+      return this.$store.state.products;
+    },
+  },
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,19 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    hello: "Hello world",
+    products: [
+      { name: "Banana Skin", price: 20 },
+      { name: "Banana aa", price: 40 },
+      { name: "Banana bb", price: 50 },
+      { name: "Banana cc", price: 60 },
+    ],
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: {},
+});
